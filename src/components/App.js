58fdf4534b8190fd7291048920c821a747f41cde
Tong Tang <pichip22@hotmail.com>
2023-06-11 23:52:05 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+    const [dog,setDog] = useState(null);
+
+    useEffect(function (){
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            return setDog(data.message);
+        });
+    },[])
+    if (!dog) return <p>Loading...</p>
+
+    return (
+        <img src={dog} alt="A Random Dog"/>
+    )
+}
+
+
+export default App
